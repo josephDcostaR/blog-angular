4ff974f9b65cb122e27971ae7557bff400d573c9
Joseph da costa ribeiro <87831574+josephDcostaR@users.noreply.github.com>
2024-01-30 20:39:25 -0300

    big-card-changes

--- angular-blog/src/app/app.component.ts
+++ angular-blog/src/app/app.component.ts
@@ -3,13 +3,14 @@ import { RouterOutlet } from '@angular/router';
 import { SmallCardComponent } from "./components/small-card/small-card.component";
 import { BigCardComponent } from "./components/big-card/big-card.component";
 import { MenuTitleComponent } from "./components/menu-title/menu-title.component";
+import { HomeComponent } from "./pages/home/home.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, SmallCardComponent, BigCardComponent, MenuTitleComponent]
+    imports: [RouterOutlet, SmallCardComponent, BigCardComponent, MenuTitleComponent, HomeComponent]
 })
 export class AppComponent {
   title = 'angular-blog';
